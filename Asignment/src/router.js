fe88b8e77fffb6a23c67ca5edb49349c2203fe39
duchc003 <duchc03.developer@gmail.com>
2023-02-07 17:53:21 +0700

    no message

--- Asignment/src/router.js
+++ Asignment/src/router.js
@@ -1,2 +1,23 @@
 var myApp = angular.module("myModule", ["ngRoute"]);
-myApp.config(function () {});
+myApp.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix();
+  $routeProvider
+    .when("/home", {
+      templateUrl: "pages/trang-chu.html",
+    })
+    .when("/gio-hang", {
+      templateUrl: "pages/gio-hang.html",
+    })
+    .when("/dang-nhap", {
+      templateUrl: "pages/dang-nhap.html",
+    })
+    .when("/dang-ki", {
+      templateUrl: "pages/dang-ki.html",
+    })
+    .when("/lien-he", {
+      templateUrl: "pages/lien-he.html",
+    })
+    .otherwise("/home", {
+      redirecTo: "pages/trang-chu.html",
+    });
+});
